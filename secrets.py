820e991b070184025cc2664333ac346ede52ea4b
v-rossini <72073804+v-rossini@users.noreply.github.com>
2020-11-18 14:58:45 -0300

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,4 +1,7 @@
 # credenciasi para a API do Spotify
+# Podem ser obtidos na "visão geral da conta" na página do Spotify
+# e em developer.spotify.com/console
+
 # obs: inclui informações da conta
 spotify_token = ""
 spotify_user_id = ""
